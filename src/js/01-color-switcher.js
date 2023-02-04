23bb167411c260ea3efbb2b327c260ca6130ca38
OleksandrRusnak <111389426+OleksandrROV@users.noreply.github.com>
2023-02-04 17:58:58 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+btnStart.addEventListener('click', changeColor);
+
+btnStop.addEventListener('click', stopChangeColor);
+
+function changeColor() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangeColor() {
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+  clearInterval(timerId);
+}
